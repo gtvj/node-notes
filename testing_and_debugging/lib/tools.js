b22104a10c730485106b9e69537ec93313ae946b
Gwyn Jones <m@gwnjns.com>
2018-05-11 08:19:17 +0100

    Add example of asynchonous test
    This test makes an https call to Wikipedia and makes an assertion
    against the text that is returned. A few things to note here:
    
    - the inclusion of 'done' as an argument to the 'it' callback. Mocha
      will only consider the test complete once the 'done' callback has
      been called. See https://mochajs.org/#asynchronous-code
    - the BDD style chaining of expect() within the test.

--- testing_and_debugging/lib/tools.js
+++ testing_and_debugging/lib/tools.js
@@ -1,6 +1,27 @@
+let https = require('https');
+
 let tools = {
     sayHi() {
         return 'hello';
+    },
+
+    loadWiki(string, callback) {
+        let url = `https://en.wikipedia.org/wiki/${string}`;
+
+        https.get(url, res => {
+
+            let body = '';
+
+            res.setEncoding('UTF-8');
+
+            res.on('data', chunk => {
+                body += chunk
+            });
+
+            res.on('end', () => {
+                callback(body)
+            });
+        })
     }
 };
 
--- testing_and_debugging/test/first-spec.js
+++ testing_and_debugging/test/first-spec.js
@@ -1,13 +1,30 @@
 let {expect} = require('chai');
 let tools = require('../lib/tools');
 
-describe("sayHi()", () => {
+describe("Testing the tools modules", function () {
 
-    it('should say hello', function () {
+    describe("sayHi()", () => {
 
-        let result = tools.sayHi();
+        it('should say hello', function () {
 
-        expect(result).to.equal('hello');
+            let result = tools.sayHi();
 
+            expect(result).to.equal('hello');
+
+        });
     });
+
+    describe("loadWiki()", function () {
+
+        it('should load Abraham Lincoln\'s wikipedia page', function (done) {
+
+            tools.loadWiki('Abraham_Lincoln', function (html) {
+                expect(html).to.be.a('string').that.includes('1864 presidential election');
+                done();
+            })
+
+        })
+
+    })
 });
+
